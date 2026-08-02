fc4a10e29820c1d6ef8a4695ece2604600f27a40
azhar <mazharaa32@gmail.com>
2026-08-02 23:13:55 +0700

    finishing step 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gojsonparser
+
+go 1.25.5
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
+/// 2>/dev/null; exec go run "$0" "$@"
+
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	filePath := os.Args[1]
+	data, e := os.ReadFile(filePath)
+
+	if e != nil {
+		fmt.Println("Error:", e)
+	}
+
+	if len(data) > 1 {
+		if string(data[0]) == "{" && string(data[len(data)-1]) == "}" {
+			fmt.Println("Valid JSON")
+			os.Exit(0)
+		} else {
+			fmt.Println("Invalid JSON")
+			os.Exit(1)
+		}
+	} else {
+		fmt.Println("Invalid JSON")
+		os.Exit(1)
+	}
 }
