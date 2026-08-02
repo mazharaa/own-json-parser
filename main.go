72ccc81066210cf5d7cf6b8d658efb951fcccd03
azhar <mazharaa32@gmail.com>
2026-08-03 00:30:10 +0700

    create standalone function for JSON validation

--- main.go
+++ main.go
@@ -7,22 +7,28 @@ import (
 	"os"
 )
 
+func parseJSON(data []byte) bool {
+	if len(data) > 1 {
+		if string(data[0]) == "{" && string(data[len(data)-1]) == "}" {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	filePath := os.Args[1]
 	data, e := os.ReadFile(filePath)
 
 	if e != nil {
 		fmt.Println("Error:", e)
+		os.Exit(1)
 	}
 
-	if len(data) > 1 {
-		if string(data[0]) == "{" && string(data[len(data)-1]) == "}" {
-			fmt.Println("Valid JSON")
-			os.Exit(0)
-		} else {
-			fmt.Println("Invalid JSON")
-			os.Exit(1)
-		}
+	if parseJSON(data) {
+		fmt.Println("Valid JSON")
+		os.Exit(0)
 	} else {
 		fmt.Println("Invalid JSON")
 		os.Exit(1)
