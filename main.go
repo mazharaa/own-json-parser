0118cd47b21ec3e7081465cf886a7705ce5a718a
mazharaa <74483294+mazharaa@users.noreply.github.com>
2026-08-06 02:03:27 +0700

    fix bugs on logic side and add more test cases for step 1 (#2)

--- main.go
+++ main.go
@@ -8,16 +8,40 @@ import (
 )
 
 func parseJSON(data []byte) bool {
-	if len(data) > 1 {
-		if string(data[0]) == "{" && string(data[len(data)-1]) == "}" {
-			return true
+	foundFirstCurl := false
+	foundPair := false
+
+	for i:=range data {
+		if string(data[i]) == " " {
+			continue
+		} else if string(data[i]) == "{" {
+			if !foundFirstCurl {
+				foundFirstCurl = true
+				continue
+			} else if foundPair{
+				foundPair = false
+				continue
+			}
+		} else if string(data[i]) == "}" {
+			if foundPair{
+				return false
+			} else if foundFirstCurl {
+				foundPair = true
+				continue
+			}
+		} else {
+			return false
 		}
 	}
-
-	return false
+	return foundPair
 }
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Error: No Argument")
+		os.Exit(1)
+	}
+	
 	filePath := os.Args[1]
 	data, e := os.ReadFile(filePath)
 
