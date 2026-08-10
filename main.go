c13de8157b61e7ee4fbd40b74bb1cfd348b938f0
azhar <mazharaa32@gmail.com>
2026-08-10 23:33:12 +0700

    completing logic for bool and int value json parser

--- main.go
+++ main.go
@@ -43,13 +43,21 @@ func parseJSON(data []byte) bool {
 				skipWhiteSpace(data[pos:], &pos)
 
 				if !expect(data, &pos, '"') {
-					return false
-				}
+					if data[pos] == byte('t') || data[pos] == byte('f') || data[pos] == byte('n') {
+						if !keywordTypeMatch(data, &pos, data[pos]) {
+							return false
+						}
+					} else {
+						if !parseNum(data, &pos, data[pos]) {
+							return false
+						}
+					}
+				} else {
+					skipChars(data, &pos)
 
-				skipChars(data, &pos)
-
-				if !expect(data, &pos, '"') {
-					return false
+					if !expect(data, &pos, '"') {
+						return false
+					}
 				}
 
 				skipWhiteSpace(data[pos:], &pos)
@@ -114,6 +122,92 @@ func expect(input []byte, pos *int, char byte) bool {
 		return false
 	}
 	*pos ++
+	return true
+}
+
+func keywordTypeMatch(input []byte, pos *int, firstChar byte) bool {
+	keywordMatch := []byte("")
+	*pos ++
+
+	if firstChar == 't' && (*pos + 2) < len(input) {
+		keywordMatch = []byte("rue")
+		
+		for _, val := range keywordMatch {
+			if input[*pos] != byte(val) {
+				return false
+			}
+			*pos ++
+		}
+	} else if firstChar == 'f' && (*pos + 3) < len(input) {
+		keywordMatch = []byte("alse")
+		
+		for _, val := range keywordMatch {
+			if input[*pos] != byte(val) {
+				return false
+			}
+			*pos ++
+		}
+	} else if firstChar == 'n' && (*pos + 2) < len(input) {
+		keywordMatch = []byte("ull")
+		
+		for _, val := range keywordMatch {
+			if input[*pos] != byte(val) {
+				return false
+			}
+			*pos ++
+		}
+	} else {
+		return false
+	}
+
+	return true
+}
+
+func parseNum(input []byte, pos *int, firstElement byte) bool {
+	countDecimal := 0
+	firstElementChecked := false
+	for *pos < len(input) {
+		if !firstElementChecked {
+			if (input[*pos] == byte('-')) || (input[*pos] >= byte('0') && input[*pos] <= byte('9')) {
+				*pos ++
+				firstElementChecked = true
+			} else {
+				return false
+			}
+		} else if input[*pos] >= byte('0') && input[*pos] <= byte('9') {
+			if firstElement == byte('0') {
+				if countDecimal != 1 {
+					return false
+				}
+			}
+			*pos ++
+		} else if input[*pos] == byte('.') {
+			if countDecimal > 0 || input[*pos - 1] == byte('-'){
+				return false
+			}
+			*pos ++
+			countDecimal ++
+		} else if (input[*pos] == byte('+') || input[*pos] == byte('-')) {
+			if input[*pos - 1] != byte('e') && input[*pos - 1] != byte('E') {
+				return false
+			}
+
+			*pos++
+		} else if input[*pos] == byte(',') || input[*pos] == byte('}') || input[*pos] == byte('\n') {
+			if input[*pos - 1] == byte('.') || input[*pos - 1] == byte('e') || input[*pos - 1] == byte('E') || input[*pos - 1] == byte('-') || input[*pos - 1] == byte('+') {
+				return false
+			}
+			break
+		} else if input[*pos] == byte('E') || input[*pos] == byte('e'){
+			if input[*pos - 1] == byte('-') {
+				return false
+			}
+			*pos ++
+		} else  {
+			return false
+		}
+	}
+
 	return true
 }
 
